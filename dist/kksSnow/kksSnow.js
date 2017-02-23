bf1e3b9a25600d55531695f23942b879b8dd1e3b
zhyuzh <zhyuzh@zhyuzhs-MacBook-Air.local>
2017-02-23 23:06:37 +0800

    snow增加舞动noise效果，调整参数改为和时间相关联

--- dist/kksSnow/kksSnow.js
+++ dist/kksSnow/kksSnow.js
@@ -1,7 +1,9 @@
 /**
- * v0.2.0
+ * v0.3.0
  * KKsMagic飘雪预设
+ * 随机舞动dance效果需要SimplexNoise和polyfill/typedarray支持
  */
+
 
 
 (function () {
@@ -23,16 +25,18 @@
         maxCount: 2000, //最大雪花数量，超过这个数量的雪花会被忽略
         count: 20, //每秒产生雪花数量，推荐60～100
         size: 1, //雪花大小，不推荐修改
-        speed: 5, //每秒向下移动数值
+        pos: '0 -30 0', //飘雪范围的中心，不推荐修改;低于y值雪花消失
+        box: '100 10 100', //生成雪花的盒子，相对于pos，范围越大需要生成越多的雪花
+        boxHeight: 90, //雪花盒子的距离地面的高度
+        speed: 10, //每秒向下移动数值，推荐5～20
+        acc: 5, //加速度，每秒变化量,生成时生效，推荐小于speed；这个值同时轻微影响dance效果
+        accRand: 2, //加速度随机变化值，生成时生效，推荐与acc相加小于speed；
+        dance: 7, //每秒飘舞幅度，值越大水平方向飘动越严重，即时生效，推荐2～10
         color: '#FFFFFF', //雪花的颜色，不推荐修改
         colors: undefined, //随机颜色，数组，将覆盖color选项。不推荐使用
         opacity: 0.66, //雪花透明度，推荐0.1～1
         textrue: path + "imgs/dot-64.png", //雪花的形状图片，不推荐修改
-        height: 50, //雪花生成的高度，不推荐修改
-        low: -20, //雪花消失的高度，不推荐修改
-        range: 100, //飘雪的范围，范围越大需要生成越多的雪花
-        pos: '0 0 0', //飘雪范围的中心，不推荐修改
-    }
+    };
 
     /**
      * 默认的初始化粒子函数,
@@ -45,13 +49,12 @@
 
         //生成基本数据
         ctx.$kksData = {
+            time: 0,
             points: [],
             colors: [],
         };
 
         genOpt.call(ctx);
-
-
 
         //生成Object3D对象
         ctx.$kksSnow = new THREE.Points(new THREE.Geometry(), ctx.$kksData.mat);
@@ -100,6 +103,12 @@
             ctx.$kksOpt.pos = new THREE.Vector3(Number(posArr[0]), Number(posArr[1]), Number(posArr[2]));
         };
 
+        //整理数据
+        if (ctx.$kksOpt.box.constructor == String) {
+            var boxArr = ctx.$kksOpt.box.split(' ');
+            ctx.$kksOpt.box = new THREE.Vector3(Number(boxArr[0]), Number(boxArr[1]), Number(boxArr[2]));
+        };
+
         //生成材质
         var mat = new THREE.PointsMaterial({
             size: ctx.$kksOpt.size,
@@ -134,14 +143,17 @@
         var kksData = ctx.$kksData;
         var kksOpt = ctx.$kksOpt;
 
-        var n = deltaTime / 1000 * kksOpt.count;
+        var timeUnit = kksData.time == 0 ? 0.16 : deltaTime / 1000;
+        var n = timeUnit * kksOpt.count;
 
         for (var i = 0; i < n; i++) {
             var p = {};
+            var x = kksOpt.pos.x + Math.random() * kksOpt.box.x - kksOpt.box.x / 2;
+            var y = kksOpt.pos.y + kksOpt.boxHeight + Math.random() * kksOpt.box.y - kksOpt.box.y / 2;
+            var z = kksOpt.pos.z + Math.random() * kksOpt.box.z - kksOpt.box.z / 2;
+            p.pos = new THREE.Vector3(x, y, z);
 
-            var x = kksOpt.pos.x + Math.random() * kksOpt.range - kksOpt.range / 2;
-            var z = kksOpt.pos.z + Math.random() * kksOpt.range - kksOpt.range / 2;
-            p.pos = new THREE.Vector3(x, kksOpt.height + kksOpt.pos.y, z);
+            p.acc = genRandomV3().multiplyScalar((kksOpt.acc + Math.random() * kksOpt.accRand));
 
             //为了避免动态调整找不到clr，无论是否开启colors都指定clr参数
             if (kksOpt.colors) {
@@ -159,11 +171,15 @@
     /**
      * 每帧都重新计算雪花的位置，生成新的物体
      */
+    var danceGen = SimplexNoise ? new SimplexNoise() : undefined;
+
     function tickSnow(deltaTime) {
         var ctx = this;
         var kksData = ctx.$kksData;
         var kksOpt = ctx.$kksOpt;
 
+        kksData.time += deltaTime;
+        var timeUnit = kksData.time == 0 ? 0.16 : deltaTime / 1000;
 
         var parr = [];
         var varr = [];
@@ -174,8 +190,20 @@
 
         for (var i = offset; i < kksData.points.length; i++) {
             var p = kksData.points[i];
-            if (p && p.pos && p.pos.y >= kksOpt.low) {
-                p.pos.setY(p.pos.y -= kksOpt.speed * deltaTime / 1000);
+            if (p && p.pos && p.pos.y >= kksOpt.pos.y) {
+
+                if (danceGen) {
+                    var ax = danceGen.noise2D(p.pos.y * 0.05, p.acc.x);
+                    var az = danceGen.noise2D(p.pos.y * 0.05, p.acc.z);
+                    var ay = danceGen.noise2D(p.pos.y * 0.05, p.acc.y);
+                    var dance = new THREE.Vector3(ax, ay, az);
+                    dance.multiplyScalar(kksOpt.dance * timeUnit);
+                    p.pos.add(dance);
+                };
+
+                p.pos.add(p.acc.clone().multiplyScalar(timeUnit));
+                p.pos.setY(p.pos.y -= kksOpt.speed * timeUnit);
+
                 parr.push(p);
                 varr.push(p.pos);
                 if (kksOpt.colors) carr.push(p.clr);
@@ -189,6 +217,28 @@
         if (kksOpt.colors || kksOpt.colors) newGeo.colors = carr;
 
         ctx.$kksSnow.geometry = newGeo;
+    };
+
+
+
+    /**
+     * 生成随机数字,正负值,-1到+1
+     * @returns {number} res
+     */
+    function genRandom() {
+        if (Math.random() > 0.5) {
+            return Math.random();
+        } else {
+            return Math.random() * -1;
+        };
+    };
+
+    /**
+     * 生成随机Vector3，正负值,-1到1
+     * @returns {number} res
+     */
+    function genRandomV3(base) {
+        return new THREE.Vector3(genRandom(), genRandom(), genRandom());
     };
 
 
--- temp/test5.js
+++ temp/test5.js
@@ -4,9 +4,6 @@
     <title>FireworksKK</title>
     <meta charset="utf-8">
     <script src="./dist/lib/jquery.min.js"></script>
-    <script src="./dist/lib//typedarray.js"></script>
-    <script src="./dist/lib/simplex-noise.js"></script>
-
     <script src="./dist/lib/aframe.min.js"></script>
     <script src="./dist/kksMagic/kksMagic.js"></script>
     <script src="./dist/kksFireWorks/kksFireWorks.js"></script>
@@ -16,7 +13,8 @@
 <body>
     <a-scene>
         <a-entity id='snow' position='-2 2 -15' kks-magic='preset:snow;options:{color:"#FFFFFF"}'></a-entity>
-<!--        <a-entity position='0 -30 -100' kks-magic='preset:fireworks;options:{eColor:"#FF3333"}'></a-entity>-->
+//        <a-entity position='0 -30 -100' kks-magic='preset:fireworks;options:{eColor:"#FF3333"}'></a-entity>
         <a-sky color='#222'></a-sky>
     </a-scene>
+    <script src="./index.js"></script>
 </body>
